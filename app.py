73e5055fe9a4283bed0bfd61435cbe8bcf32c689
dcasalv023 <plaracil3@gmail.com>
2024-03-07 00:17:43 +0100

    Revisa los errores

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-""" Docstring del módulo """
+"""Docstring del módulo"""
+
 def suma(a: int, b: int) -> int:
-""" docstring function
-# debe dar errores en linter
-"""
-x = "no se usa"
-return a+ b
+    """docstring de la función"""
+    # Esta línea debería dar errores en el linter porque x no se utiliza.
+    x = "no se usa"
+    return a + b
+
 
 #Seguimos para bingo.
